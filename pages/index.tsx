ca30ca2f387e4e5438742497e5b7ecbf3fb4db00
Florian LEROUX <florian.leroux3@laposte.net>
2022-02-08 22:37:55 +0100

    init(tailwind): add tailwind integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return <div className="text-6xl p-6 font-bold">Hello World</div>;
 };
 
 export default Home;
