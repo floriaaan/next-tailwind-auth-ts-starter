c04124045a8bf73ca0434d0bef9861bdfca21181
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-14 14:06:36 +0200

    ci: build failed

--- e2e/index.test.ts
+++
@@ -1,6 +0,0 @@
-// import { test, expect } from "@playwright/test";
-
-// test("should contain h1", async ({ page }) => {
-//   await page.goto("/");
-//   await expect(page.locator("h1")).toContainText("Make your website wonderful");
-// });
