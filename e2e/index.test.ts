c8cfdd6ef88fc17c47ef6f86d6d135625c6e949f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-14 13:53:19 +0200

    ci: remove pw

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,6 +1,6 @@
-import { test, expect } from "@playwright/test";
+// import { test, expect } from "@playwright/test";
 
-test("should contain h1", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.locator("h1")).toContainText("Make your website wonderful");
-});
+// test("should contain h1", async ({ page }) => {
+//   await page.goto("/");
+//   await expect(page.locator("h1")).toContainText("Make your website wonderful");
+// });
